90863753bb8853b9a42e8b8af01d299dc71e6433
Nippun <nippunsingla07@gmail.com>
2025-03-01 14:57:32 -0800

    test

--- frontend/src/hooks/useValidate.jsx
+++ frontend/src/hooks/useValidate.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-const useValidate = () => {
+const  useValidate = () => {
     const navigate = useNavigate();
 
 useEffect(() => {
     const fetchAuth = async () => {
-        const url = "https://strive-app-backend.onrender.com"
-        const res = await fetch(`${url}/api/v1/user/validate`, {
+        const res = await fetch('/api/v1/user/validate', {
             method: 'GET',
             credentials: 'include', // Send cookies with the request
         });
