f21d3dc79b93d6f437e839fa3972eacd6a757f91
Nippun <nippunsingla07@gmail.com>
2025-03-06 13:29:02 -0800

    test

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -9,7 +9,15 @@ const SuggestedUsers = () => {
     useEffect(() => {
         const fetchSuggestedUsers = async () => {
             try {
-                const response = await fetch(`${import.meta.env.VITE_API_URL}/v1/user/suggested`);
+                const token = localStorage.getItem("token");
+
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/v1/user/suggested`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`,  // Add token to Authorization header
+                        "Content-Type": "application/json"   // Optional, but good practice
+                    }
+                });
                 const data = await response.json();
                 if (data.success) {
                     setUsers(data.users);
@@ -22,16 +30,16 @@ const SuggestedUsers = () => {
         fetchSuggestedUsers();
     }, []);
 
-    const goToProfile =(id) => {
+    const goToProfile = (id) => {
         console.log(id);
-        navigate(`/profile/${id}`, {state: {id}});
+        navigate(`/profile/${id}`, { state: { id } });
     }
 
     return (
         <div className="suggestedUers w-full">
             <ul className='w-full'>
                 {users.map(user => (
-                    <li onClick={()=>{goToProfile(user._id)}} key={user._id} className='flex items-center hover:bg-gray-200 w-full p-2 gap-2 rounded-md border-b cursor-pointer'>
+                    <li onClick={() => { goToProfile(user._id) }} key={user._id} className='flex items-center hover:bg-gray-200 w-full p-2 gap-2 rounded-md border-b cursor-pointer'>
                         <img src={user.profilePicture} className="w-8 h-8 mr-2 rounded-full" alt={`${user.username}'s profile`} />
                         <span>{user.username}</span>
                     </li>
