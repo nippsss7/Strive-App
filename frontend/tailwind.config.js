fd64d7df4a8233bdfb06646e567eef636a838a0c
Nippun <nippunsingla07@gmail.com>
2025-03-17 19:29:41 -0700

    font fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			// lg: 'var(--radius)',
-  			// md: 'calc(var(--radius) - 2px)',
-  			// sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {},
-		fontFamily: {
-			kumbh: ['Kumbh Sans', 'sans-serif'],
-			poppins: ['Poppins', 'sans-serif']
-		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+	theme: {
+		extend: {
+			borderRadius: {
+				// lg: 'var(--radius)',
+				// md: 'calc(var(--radius) - 2px)',
+				// sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {},
+			fontFamily: {
+				kumbh: ['Kumbh Sans', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif']
+			},
+		},
+	},
+	safelist: [
+		'font-kumbh', // Safelist the 'font-kumbh' class to prevent purging in production
+	],
+	plugins: [require("tailwindcss-animate")],
 }
