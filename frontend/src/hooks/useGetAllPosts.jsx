02b0ced91b62b9bacbff987029e8f1fbd975f8ce
Nippun <nippunsingla07@gmail.com>
2025-05-25 21:56:39 -0700

    added token authorization token

--- frontend/src/hooks/useGetAllPosts.jsx
+++ frontend/src/hooks/useGetAllPosts.jsx
@@ -1,12 +1,15 @@
 import { setPosts } from '@/redux/postSlice'
+import { useAuth } from '@clerk/clerk-react'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 const useGetAllPosts = () => {
     const dispatch = useDispatch()
+    const { getToken, isSignedIn } = useAuth();
 
     useEffect(() => {
         const fetchAllPost = async () => {
+            const token = await getToken();
             try {
                 const res = await fetch(`${import.meta.env.VITE_API_URL}/v1/post/all`, {
                     method: 'GET',
